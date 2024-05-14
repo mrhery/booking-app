f7b164a61b452cf2bd6f61c5c201e54569d5023d
AriffHakemi <ariffkamal60@gmail.com>
2024-05-14 12:35:32 +0800

    asda

---
+++ Booking-app/www/api.php
@@ -0,0 +1,63 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+header("Content-Type: application/json");
+
+$conn = mysqli_connect("127.0.0.1", "root", "", "cloudtunai");
+
+if(!$conn){
+	die(json_encode([
+		"status"	=> "error",
+		"message"	=> "Fail connect to database."
+	]));
+}
+
+
+if(isset($_GET["key"]) && $_GET["key"] == "asdljnalsdasd"){
+	if(isset($_GET["action"])){
+		switch($_GET["action"]){
+			case "login":
+				if(isset($_POST["username"], $_POST["password"])){
+					$salt = '5a7347f6fda4a346760af782d2ec126f7b9873ea9a*&(*9yad09707d0a7d0ad!@#!@#!#!#!$#!$!$!#$!$!$!$!$!%@$%#&&*^(7f2bb1fee9abdfd5f4dfc9';
+					$string = $_POST["password"];
+					 
+					$pass = hash("sha256", $string);
+					
+					$q = mysqli_query($conn, "SELECT * FROM users 
+						WHERE u_email = '{$_POST["username"]}' AND
+						u_password = '$string'
+					");
+					
+					$n = mysqli_num_rows($q);
+					
+					if($n > 0){
+						die(json_encode([
+							"status"	=> "success",
+							"message"	=> "Login in success"
+						]));
+					}else{
+						die(json_encode([
+							"status"	=> "error",
+							"message"	=> "Username or password is incorrect"
+						]));
+					}
+				}else{
+					die(json_encode([
+						"status"	=> "error",
+						"message"	=> "Insufficient request paramater"
+					]));
+				}
+			break;
+		}
+	}else{
+		die(json_encode([
+			"status"	=> "error",
+			"message"	=> "Unknown API endpoint."
+		]));
+	}
+}else{
+	die(json_encode([
+		"status"	=> "error",
+		"message"	=> "API Key is invalid."
+	]));
+}
